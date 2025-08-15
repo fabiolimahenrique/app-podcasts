980b152938764e0240de1e4426303ccdcc8d17e7
Fabio Lima <fabio.lima.henrique@gmail.com>
2025-08-15 08:00:47 -0300

    Criação servidor http

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("olá")
+import * as http from "http";
+
+const server = http.createServer( (req:  http.IncomingMessage , res: http.ServerResponse) => {
+    
+})
+
+const PORTA = process.env.PORT;
+
+server.listen(PORTA, () => {
+  console.log(`Servidor iniciado na porta: ${PORTA} `)
+});
